6dee32710f32b698e25da57baaef2a441625f611
Numonovdev <ebxuzsher@gmail.com>
2024-06-26 22:22:50 +0300

    sta

---
+++ js/index.js
@@ -0,0 +1,108 @@
+
+// Obyektlar aloqador masalala.
+
+// 1-masala
+// const person = {
+//      name: 'Sherniyoz',
+//      old: 23,
+//      job: 'teacher',
+// }
+
+// console.log(person)
+
+// 2-masala
+
+// const person = {
+//      name: 'Ali',
+//      old: 32,
+//      job: 'blogger',
+// }
+// person.name = 'vali',
+// console.log(person)
+
+
+// 3-masala
+
+// const person = {
+//      name: 'Marjona',
+//      old: 19,
+//      job: 'student',
+// }
+// person.from = 'Quva',
+// console.log(person)
+
+
+// 4-masala
+
+// const person = {
+//      name: 'Marjona',
+//      old: 19,
+//      job: 'student',
+//      from: 'Quva',
+// }
+// delete person.old
+// console.log(person)
+
+
+// 5-masala
+// const person = {
+//      name: 'Marjona',
+//      old: 19,
+//      job: 'student',
+//      from:  'Quva',
+//      telefon: {
+//           mobile: 998999821332,
+//           homePhone: 998907798101, 
+//      }
+// }
+// console.log(person)
+
+
+// 7-masala
+
+// const person = {
+//      name: 'Sherniyoz',
+//      old: 23,
+//      job: 'teacher',
+// }
+
+// console.log(person.old)
+
+// 8-masala
+
+
+
+// Umumlashtiruvcchi masalalar
+
+// 1-masala
+// let raqamlar = '32152345732336235';
+// let value = raqamlar;
+//      value.map(function(element){
+//           let nechta = 0;
+//           if(element = 3){
+//               nechta = nechta + 1;
+//           }
+//           return nechta
+//      });
+// console.log(nechta);
+
+
+// 2-masala
+// sonN = [50]
+// function element(sonN) {
+//      let raqam = [];
+//      for (let i = 1; i < sonN; i += 2) {
+//          raqam.push(i);
+//      }
+//      return raqam;
+//  }
+//  console.log(element(sonN));  
+
+
+// 3-masala
+
+
+
+
+// let arr = ['car', 'mevalar', 'sabzovotlar', 'uylar']
+
